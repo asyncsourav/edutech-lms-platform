4d8013d5460bdce158952358a0ce493dfc74ba06
Sourav Kumar <xsouravkumar357@gmail.com>
2026-01-11 17:05:29 +0530

    connnecting user routes and enhancing logic for connectDB

--- server/index.js
+++ server/index.js
@@ -3,24 +3,34 @@ import express from "express";
 import connectDB from "./src/config/db.js";
 import cookieParser from "cookie-parser";
 import { ENV } from "./src/config/env.js";
+import userRoute from "./src/routes/user.route.js"
+
 
 
 // setting up the server and using middleware
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+
+
+
+// routing the api
+app.use("/api", userRoute);
+
 
 
 
 // connect db and starting server
-app.listen(ENV.PORT, () => {
-    console.log(`Server started at port ${ENV.PORT}....`);
-    
-    // connecting to database after server starts
-    connectDB().catch((err) => {
-            console.error({ message: "Error connecting to the database", error: err });
-            console.log("Server is running without database connection...");
+connectDB()
+    .then(() => {
+        // starting server after db connection 
+        app.listen(ENV.PORT, () => {
+            console.log(`Server running on port ${ENV.PORT}`);
         });
-});
+    })
+    .catch(err => {
+            console.error({ message: "Error connecting to the database", error: err });
+        process.exit(1);
+    });
 
