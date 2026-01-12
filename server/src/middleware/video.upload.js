70377d4501a5153e73256a4bfab3679a190bc8c2
Sourav Kumar <xsouravkumar357@gmail.com>
2026-01-12 22:32:23 +0530

    updated logic for video uploader

--- server/src/middleware/video.upload.js
+++ server/src/middleware/video.upload.js
@@ -7,18 +7,19 @@ import cloudinary from "../config/cloudinary.js";
 
 
 const storage = new CloudinaryStorage({
-    cloudinary:cloudinary,
-    params:{
-        folder:"courseModule",
-        resource_type:'video',
-        allowed_formats:['mp4', 'mov', 'avi']
+    cloudinary: cloudinary,
+    params: {
+        folder: "courseModule",
+        resource_type: 'video',
+        allowed_formats: ['mp4', 'mov', 'avi']
     }
 });
 
+
 export const videoUpload = multer({
-    storage:storage,
-    limits:{
-        fileSize:1024*1024*500 // this will upload 500gb file
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 500 // this will upload 500gb file
     }
 });
 
